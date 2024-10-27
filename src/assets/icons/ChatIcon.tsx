7e931f638c54165774f5d6c0f6799cdbdbca9d2b
MAGINO DANIEL <maginodan@gmail.com>
2024-10-28 01:52:24 +0300

    updates

--- src/assets/icons/ChatIcon.tsx
+++ src/assets/icons/ChatIcon.tsx
@@ -1,22 +1,53 @@
+// import React from 'react';
+
+// const ChatIcon = () => {
+//   return (
+//     <svg
+//       stroke='currentColor'
+//       fill='none'
+//       strokeWidth='2'
+//       viewBox='0 0 24 24'
+//       strokeLinecap='round'
+//       strokeLinejoin='round'
+//       className='h-4 w-4'
+//       height='1em'
+//       width='1em'
+//       xmlns='http://www.w3.org/2000/svg'
+//     >
+//       <path d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'></path>
+//     </svg>
+//   );
+// };
+
+// export default ChatIcon;
+
 import React from 'react';
 
-const ChatIcon = () => {
+const LegalIcon = () => {
   return (
     <svg
-      stroke='currentColor'
-      fill='none'
-      strokeWidth='2'
-      viewBox='0 0 24 24'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      className='h-4 w-4'
-      height='1em'
-      width='1em'
-      xmlns='http://www.w3.org/2000/svg'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      className="h-4 w-4"
     >
-      <path d='M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'></path>
+      {/* Document Path */}
+      <path
+        d="M9 2h6a2 2 0 0 1 2 2v4h-2V4H9v16h3v2H7a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z"
+      />
+      {/* Handshake Path */}
+      <path
+        d="M21.5 17.5a3 3 0 0 0-4.5-2.6 3 3 0 0 0-4.5 2.6 3 3 0 0 0 3 3h3.5a3 3 0 0 0 3-3z"
+      />
+      <path
+        d="M15.5 21.5l3.5-3.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   );
 };
 
-export default ChatIcon;
+export default LegalIcon;
