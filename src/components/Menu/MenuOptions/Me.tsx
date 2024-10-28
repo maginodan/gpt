4c4a59434f0fba44e4137de06037fb152b41d8b4
maginodan <137385172+maginodan@users.noreply.github.com>
2024-10-28 13:05:24 +0300

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -16,7 +16,7 @@ const Me = () => {
     // </a>
     <a
   className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-  href='https://github.com/ztjhz/BetterChatGPT'
+  href='#'
   target='_blank'
 >
   <HeartIcon />
